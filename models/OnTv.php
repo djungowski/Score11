741e0d7d4525c16770bf05484914ab384722386e
Dominik Jungowski <Dominik@Jungowski.de>
2011-12-07 22:53:24 +0100

    TV Liste

--- models/OnTv.php
+++ models/OnTv.php
@@ -8,11 +8,28 @@ class OnTv extends Api\Transformator
     {
         $onTv = $this->getApi()->get();
         $config = \Zend_Registry::get('config');
-        foreach ($onTv as $key => $movie) {
-            $timestamp = strtotime($movie['date']);
-            // Verwendete Datumsformate erstellen
-            $onTv[$key]['timestamp-day'] = strftime($config->dates->listbox->title, $timestamp);
-            $onTv[$key]['timestamp-time'] = strftime($config->dates->listbox->time, $timestamp);
+        $router = $this->getFrontController()->getRouter();
+        $tvMovies = array();
+        
+        foreach ($onTv as $date => $movies) {
+            $timestamp = strtotime($date);            
+            foreach($movies as $key => $movie) {
+                $movies[$key]['movielink'] = $router->assemble(
+                    array(
+                        'movieid' => $movie['ID'],
+                        'name' => $movie['movietitle']
+                    ),
+                    'moviepage'
+                );
+                $timestamp = strtotime($movie['date']);
+                // Verwendete Datumsformate erstellen
+                $movies[$key]['timestamp-time'] = strftime($config->dates->listbox->time, $timestamp);
+            }
+            $tvMovies[] = array(
+                'date' => strftime($config->dates->listbox->title, $timestamp),
+                'movies' => $movies
+            );
         }
+        return $tvMovies;
     }
 }
