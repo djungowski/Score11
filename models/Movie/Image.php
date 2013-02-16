3e79611cd260ad65c66ad2e8d82c50f091a71590
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-16 17:47:25 +0100

    Erster Fall

--- models/Movie/Image.php
+++ models/Movie/Image.php
@@ -3,5 +3,8 @@ namespace Score11\Models\Movie;
 
 class Image
 {
-	
+	public function getDefaultImageIfNecessary()
+	{
+		return 'img/logo-movie.png';
+	}
 }
--- tests/Score11/Movie/ImageTest.php
+++ tests/Score11/Movie/ImageTest.php
@@ -10,4 +10,15 @@ class ImageTest extends \PHPUnit_Framework_TestCase
 		$image = new Image();
 		self::assertInstanceOf('Score11\Models\Movie\Image', $image);
 	}
+	
+	public function testGetDefaultImageIfNecessaryTrue()
+	{
+		$image = new Image();
+		$expected = 'img/logo-movie.png';
+		$movie = array(
+			'hasimage' => 'n'
+		);
+		$actual = $image->getDefaultImageIfNecessary($movie['hasimage']);
+		self::assertSame($expected, $actual);
+	}
 }
