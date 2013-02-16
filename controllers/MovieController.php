0d04d43ac9f6b4085a7d0bb306499b835244c037
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-16 17:37:21 +0100

    Score11 Standardbild verwenden

--- controllers/MovieController.php
+++ controllers/MovieController.php
@@ -1,4 +1,8 @@
 <?php
+require_once MODELSPATH . '/Movie.php';
+
+use Score11\Models\Movie;
+
 require_once LIBPATH . '/Score11/Api/Call.php';
 
 use Score11\Api;
@@ -7,5 +11,13 @@ class MovieController extends Zend_Controller_Action
 {
 	public function indexAction()
 	{
+		$movieId = (int)$this->_getParam('movieid');
+		
+		$api = new Api\Call('movie/' . $movieId);
+		$transformator = new Movie();
+		$transformator->setFrontController($this->getFrontController());
+		$transformator->setApi($api);
+		
+		$this->view->movie = $transformator->transform();
 	}
 }
