0359729f9c7506f8cc70eeb7e10c1bf559354778
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-17 04:35:28 +0100

    Grundlegender Lademechanismus

--- public/js/jquery.commentloader.js
+++ public/js/jquery.commentloader.js
@@ -8,32 +8,55 @@
 			prev,
 			next,
 			offset,
-			urlOptions;
+			urlOptions,
+			loaderFunction,
+			commentContainer;
 		
-		from = $(this).find(".from").html();
-		to = $(this).find(".to").html();
+		from = $(this).find(".from");
+		to = $(this).find(".to");
 		loader = $(options.loader);
 		prev = $(this).find(".prev");
 		next = $(this).find(".next");
+		commentContainer = $(this).find('#comments');
 		
+		jQuery.fn.commentloader.checkPrevAndNext(prev, next, from.html(), to.html(), options);
+		
+		loaderFunction = function() {
+			urlOptions = {
+				offset: options.offset,
+				limit: options.stepSize
+			};
+
+			loader.show();
+			// Initial laden
+			commentContainer.load(options.baseUrl, urlOptions, function(){
+		        loader.hide();
+		        from.html(options.offset + 1);
+		        to.html(options.offset + options.stepSize);
+		        
+		        options.offset += options.stepSize;
+		        jQuery.fn.commentloader.checkPrevAndNext(prev, next, from.html(), to.html(), options);
+		    });
+		}
+		
+		loaderFunction();
+		next.on('click', loaderFunction);
+		prev.on('click', loaderFunction);
+	};
+	
+	jQuery.fn.commentloader.checkPrevAndNext = function(prev, next, from, to, options) {
 		if (to < options.total) {
 			next.show();
+		} else {
+			next.hide();
 		}
-		
-		if (from > options.stepSize) {
-			prev.show;
+
+		if (to > options.total) {
+			prev.show();
+		} else {
+			prev.hide();
 		}
-		
-		urlOptions = {
-			offset: options.offset,
-			limit: options.stepSize
-		};
-		// Initial laden
-		$(this).find('#comments').load(options.baseUrl, urlOptions, function(){
-	        loader.hide();
-	    });
-	    
-	};
+	}
 	
 	jQuery.fn.commentloader.options = {
 		baseUrl: null,
