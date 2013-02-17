e4281a5c7616e7c6ac22b921d20acbe28dc4d6c5
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-17 01:38:27 +0100

    Mobiles Layout + Cast anzeigen

--- public/js/jquery.infotoggle.js
+++ public/js/jquery.infotoggle.js
@@ -1,12 +1,18 @@
 (function(jQuery, $) {
 	jQuery.fn.infotoggle = function(options) {
-		options = options || {};
+		options = jQuery.extend({}, jQuery.fn.infotoggle.options, options);
 		
 		var originalElement = this;
 		this.on('click', function(event) {
 			event.preventDefault();
 			originalElement.hide();
+			$(options.hideAdditionally).hide();
 			$(options.elements).show(400);
 		});
 	};
+	
+	jQuery.fn.infotoggle.options = {
+		elements: null,
+		hideAdditionally: null
+	}
 })(jQuery, jQuery);
