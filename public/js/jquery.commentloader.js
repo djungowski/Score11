d86187059556857a42f7ae367e76755469a3c063
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-17 04:59:19 +0100

    Kommentare seitenweise laden

--- public/js/jquery.commentloader.js
+++ public/js/jquery.commentloader.js
@@ -21,7 +21,10 @@
 		
 		jQuery.fn.commentloader.checkPrevAndNext(prev, next, from.html(), to.html(), options);
 		
-		loaderFunction = function() {
+		loaderFunction = function(event) {
+			if (typeof event != 'undefined' && event.data.offset == 'decrease') {
+				options.offset -= options.stepSize * 2;
+			}
 			urlOptions = {
 				offset: options.offset,
 				limit: options.stepSize
@@ -33,15 +36,21 @@
 		        loader.hide();
 		        from.html(options.offset + 1);
 		        to.html(options.offset + options.stepSize);
-		        
-		        options.offset += options.stepSize;
+		        if (to.html() > options.total) {
+		        	to.html(options.total);
+		        }
+		       
+	        	options.offset += options.stepSize;
+		        if (options.offset < 0) {
+		        	options.offset = 0;
+		        }
 		        jQuery.fn.commentloader.checkPrevAndNext(prev, next, from.html(), to.html(), options);
 		    });
 		}
-		
+
 		loaderFunction();
-		next.on('click', loaderFunction);
-		prev.on('click', loaderFunction);
+		next.on('click', {offset: 'increase'}, loaderFunction);
+		prev.on('click', {offset: 'decrease'}, loaderFunction);
 	};
 	
 	jQuery.fn.commentloader.checkPrevAndNext = function(prev, next, from, to, options) {
@@ -51,7 +60,7 @@
 			next.hide();
 		}
 
-		if (to > options.total) {
+		if (to > options.stepSize) {
 			prev.show();
 		} else {
 			prev.hide();
